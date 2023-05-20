98d4ae509732cb4c9279e16cb1216ba27d1b5d03
Piyushency <piyush.k@encycg.com>
2023-05-21 00:21:08 +0530

    Add project files.

---
+++ RateLimiter/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace RateLimiterProject
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// Create a rate limiter with a limit of 5 requests per second
+			RateLimiter rateLimiter = new RateLimiter(5, 1);
+
+			// Simulate some requests
+			for (int i = 0; i < 20; i++)
+			{
+				if (rateLimiter.IsRequestAllowed())
+				{
+					Console.WriteLine("Request allowed");
+				}
+				else
+				{
+					Console.WriteLine("Request blocked");
+				}
+
+				// Sleep for 200 milliseconds between requests
+				Thread.Sleep(100);
+			}
+		}
+	}
+}
